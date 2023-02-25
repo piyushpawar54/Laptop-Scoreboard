cbc58695e51dfd831927b9a435c101bc05bd93de
Piyush Pawar <piyushpawar199954@gmail.com>
2023-02-25 15:31:41 +0530

    DONE

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,7 @@
 import {
+  Text,
+  Html,
+  ContactShadows,
   PresentationControls,
   Float,
   Environment,
@@ -24,9 +27,37 @@ export default function Experience() {
         config={{ mass: 2, tension: 400 }}
       >
         <Float rotationIntensity={0.4}>
-          <primitive object={computer.scene} position-y={-1.2} />
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={'#ff6900'}
+            rotation={[-0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+          <primitive object={computer.scene} position-y={-1.2}>
+            <Html
+              transform
+              wrapperClass="htmlScreen"
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src="https://piyushpawar54.github.io/index.html" />
+            </Html>
+          </primitive>
+          <Text
+            font="./bangers-v20-latin-regular.woff"
+            fontSize={1}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+          >
+            LEADER BOARD
+          </Text>
         </Float>
       </PresentationControls>
+      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
     </>
   );
 }
